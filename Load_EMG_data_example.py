0fbe92dee2f72e31c1e1fd79d2e36fa6da6a1520
EleBern <bernasconi@cs.cas.cz>
2026-05-26 14:11:35 +0200

    Update for new data structure

--- Load_EMG_data_example.py
+++ Load_EMG_data_example.py
@@ -7,36 +7,45 @@ plotting is done via matplotlib, additional smoothing can be done via scipy.ndim
 import numpy as np
 import h5py
 import os
-from scipy.ndimage import gaussian_filter1d
 import matplotlib.pyplot as plt
-# hdf5_path = 'C:\\Users\\User\\nextcloud\\TMS Neuro Projects\\M1_modeling\\DI_wave_data\\DIwaves_Di_Lazzaro'
-hdf5_path = 'data_diLazzaro/DiLazarro_di_wave_data.hdf5'
 
+def plot_emg(hdf5_path, orientation, threshold, year):
+    # test if file exists in location
+    if not os.path.exists(hdf5_path):
+        raise ValueError('hdf5_path does not exist')
 
+    with h5py.File(hdf5_path, 'r') as h5file:
+        name_h5group = h5file[str(year)][orientation]['RMT']
 
-measurement_dict = dict(orientation='PA', threshold=140, year=2020, hdf5_path=hdf5_path, sigma=1.0)
+        name_keys = list(name_h5group.keys())
+        if threshold == None:
+            threshold = np.array(list(h5file['2020']["PA"]["RMT"][name_keys[0]]["intensities"]))
+            th_idx = np.arange(len(threshold))
+        else:
+            th_idx = np.argwhere(np.array(list(h5file['2020']["PA"]["RMT"][name_keys[0]]["intensities"])) == 140)[0]
+        emg_data_full = np.array(name_h5group[name_keys[0]]['EMG']['mep'])
+        emg_data_mean = np.mean(emg_data_full, axis=2)   
+        time = np.array(name_h5group[name_keys[0]]['time'])
 
-# test if file exists in location
-if not os.path.exists(hdf5_path):
-    raise ValueError('hdf5_path does not exist')
+    for i in th_idx:
+        plt.figure()
+        plt.plot(time[i, :], emg_data_mean[i, :], label='mean')
+        for j in range(emg_data_full.shape[-1]):
+            if j ==0:
+                # add name for legend on first occurence
+                plt.plot(time[i, :], emg_data_full[i, :, j], c='k', alpha=0.3, zorder=-1, label='trials')
+            else:
+                plt.plot(time[i, :], emg_data_full[i, :, j], c='k', alpha=0.3, zorder=-1)
+        plt.xlim([min(time[i]), max(time[i])])
+        plt.xlabel('time ( ms)')
+        plt.ylabel('EMG (mV)')
+        if isinstance(threshold, int):
+            title_string = 'Year ' + str(year) + ' th ' + str(threshold)
+        else:
+            title_string = 'Year ' + str(year) + ' th ' + str(threshold[i])
+        plt.title(title_string)
+        plt.legend()
+        plt.show()
 
-with h5py.File(hdf5_path, 'r') as h5file:
-    name_h5group = h5file[measurement_dict['orientation']]['RMT'][str(measurement_dict['threshold'])][
-        str(measurement_dict['year'])]
-    name_keys = list(name_h5group.keys())
-    # pick first name here (index 0) since in the dataset there is only one recording per orientation/RMT/year combination
-    emg_data_full = np.array(name_h5group[name_keys[0]]['EMG']['signal_full'])
-    emg_data_mean = np.array(name_h5group[name_keys[0]]['EMG']['signal_mean'])
-    time = np.array(name_h5group[name_keys[0]]['time'])
-emg_data_full = emg_data_full[0].T # formatting from the original .mat files into a useful shape
-plt.plot(time, emg_data_mean, label='mean')
-for i in range(emg_data_full.shape[0]):
-    if i ==0:
-        # add name for legend on first occurence
-        plt.plot(time,emg_data_full[i], c='k', alpha=0.3, zorder=-1, label='trials')
-    else:
-        plt.plot(time, emg_data_full[i], c='k', alpha=0.3, zorder=-1)
-plt.xlabel('time ( ms)')
-plt.ylabel('EMG (mV)')
-plt.legend()
-plt.show()
+if __name__ == '__main__':
+    plot_emg()
