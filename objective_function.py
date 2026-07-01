175dcb9a4221f34e319cf049af6ce3b7facbfc6d
EleBern <bernasconi@cs.cas.cz>
2026-07-01 10:45:47 +0200

    Create objective function for pheno model

--- objective_function.py
+++ objective_function.py
@@ -1,10 +1,20 @@
 
 from MEPmodel_bio import MEPmodel_bio
 
-def objective_function(p, ref):
+def objective_function_bio(p, ref):
     """
     Objective function: runs the biological MEP model and returns the residual.
     """
     _, ref_updated = MEPmodel_bio(p, ref)
+    error = ref_updated['error']
+    return error, ref_updated
+
+from MEPmodel_pheno import MEPmodel_pheno
+
+def objective_function_pheno(p, ref):
+    """
+    Objective function: runs the biological MEP model and returns the residual.
+    """
+    _, ref_updated = MEPmodel_pheno(p, ref)
     error = ref_updated['error']
     return error, ref_updated
