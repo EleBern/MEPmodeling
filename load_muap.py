23e5d2a9bc58ae9dfd1b72774c90910a948d3748
EleBern <bernasconi@cs.cas.cz>
2026-08-21 12:31:29 +0200

    Find best fit for amplitude distribution

--- load_muap.py
+++ load_muap.py
@@ -1,11 +1,12 @@
 import os
-import numpy as np
 import h5py
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 from h5_helpers import load_h5_to_dict
 
 
-def load_muap(plotOn=0):
+def load_muap(plotOn=0, amplitudeDist=0):
     """
     Load motor unit action potentials (MUAPs) 
 
@@ -29,7 +30,8 @@ def load_muap(plotOn=0):
         muaps = tmp["muaps"]   # [n_samples x n_muaps]
         t     = tmp["t"].T     # [n_samples x 1]
 
-    amplitude_distribution(muaps)
+    if amplitudeDist:
+        amplitude_distribution(muaps)
 
     if plotOn:
         fig = plt.figure()
@@ -52,13 +54,28 @@ def load_muap(plotOn=0):
 
 def amplitude_distribution(muaps):
     max_peak = np.max(muaps, axis=0)
+    print(np.max(max_peak))
     min_peak = np.min(muaps, axis=0)
     amplitude = 1e3 * np.sort(max_peak - min_peak)
+    popt = fit_amplitude(amplitude)
     fig = plt.figure()
     print(np.shape(max_peak))
-    plt.plot(np.arange(len(amplitude)), amplitude, "*")
+    plt.plot(np.arange(len(amplitude)), amplitude, "*", label="MUAP amplitude")
+    plt.plot(np.arange(len(amplitude)), exponential(np.arange(len(amplitude)) / (len(amplitude)-1), *popt), "r", label="Best fit")
     plt.title("MUAPs amplitude distribution")
     plt.ylabel("Amplitude [mV]")
     plt.xlabel("Motor unit sorted index")
     plt.xlim([0, 100])
+    plt.legend()
     plt.show()
+
+def exponential(x, a, b):
+    return a*b**(x)
+
+def fit_amplitude(amplitude):
+    x = np.arange(len(amplitude)) / (len(amplitude)-1)
+    #y = exponential(x, 2.5)
+    popt, pcov = curve_fit(exponential, x, amplitude, p0=[1e-5, 100])
+    print(popt)
+
+    return popt
