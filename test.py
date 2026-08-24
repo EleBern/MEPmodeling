a23fe98a340a83c879687bda748160a8200ff028
EleBern <bernasconi@cs.cas.cz>
2026-08-24 13:40:23 +0200

    Fit subj 1 pheno model synthetic muaps

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-# from ga_MEPmodel_pheno import ga_MEPmodel_pheno
-# ga_MEPmodel_pheno(1,0)
+from ga_MEPmodel_pheno import ga_MEPmodel_pheno
+ga_MEPmodel_pheno(1,1)
 
-from load_muap import load_muap
-load_muap(1, 1)
+# from load_muap import load_muap
+# load_muap(1, 1)
