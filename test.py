81b3b5497340ba9b62962e4b710fa65702081c4c
EleBern <bernasconi@cs.cas.cz>
2026-08-27 17:19:42 +0200

    Remove previously fitted results

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-from ga_MEPmodel_bio import ga_MEPmodel_bio
-ga_MEPmodel_bio(8,1,[],1)
+try:
+    from ga_MEPmodel_bio import ga_MEPmodel_bio
+    ga_MEPmodel_bio(8,1,[],1)
+except:
+    print("Biological model failed")
 
+try:
+    from ga_MEPmodel_bio import ga_MEPmodel_bio
+    ga_MEPmodel_bio(8,0,[],1)
+except:
+    print("Biological NO RC model failed")
+
+try:
+    from ga_MEPmodel_pheno import ga_MEPmodel_pheno
+    ga_MEPmodel_pheno(8,1,[],0)
+except:
+    print("Pheno model failed")
 # from load_muap import load_muap
-# load_muap(1, 1)
+# load_muap(1, 0)
--- test3.py
+++ test3.py
@@ -1,2 +1,17 @@
-from ga_MEPmodel_bio import ga_MEPmodel_bio
-ga_MEPmodel_bio(3,1,[],1)
+try:
+    from ga_MEPmodel_bio import ga_MEPmodel_bio
+    ga_MEPmodel_bio(3,1,[],1)
+except:
+    print("Biological model failed")
+
+try:
+    from ga_MEPmodel_bio import ga_MEPmodel_bio
+    ga_MEPmodel_bio(3,0,[],1)
+except:
+    print("Biological NO RC model failed")
+
+try:
+    from ga_MEPmodel_pheno import ga_MEPmodel_pheno
+    ga_MEPmodel_pheno(3,1,[],0)
+except:
+    print("Pheno model failed")
