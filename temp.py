2ece2a3a8626cbee092b11c67d2bf7f53b89400d
EleBern <bernasconi@cs.cas.cz>
2026-09-10 18:21:05 +0200

    Update logic to follow helper_f_genmuaps

--- temp.py
+++ temp.py
@@ -2,39 +2,31 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
-from zero_crossing import zero_crossings_all
+from zero_crossing import crossing_times
 from downsample_muaps import load_unprocessed_muaps
+from helper_f_genmuaps import amplitude_distribution, find_peak_times
 
 root    = os.path.dirname(os.path.realpath(__file__))
 h5_path = os.path.join(root, "data_MUAP", "Dist5_Monopolar_Rest_NormalCV_New.hdf5")
 
 t, anatomical_muaps, downsampled_t, downsampled_muaps = load_unprocessed_muaps(h5_path)
+# Calculate the zero-crossing of the anatomical MUAPs
 dt = t[1] - t[0]
-crossings = zero_crossings_all(t, anatomical_muaps)
-
+delay = crossing_times(t, anatomical_muaps)
 # Calculate the amplitude of the anatomical MUAPs
-for i in range(100):
-    # Find peaks with a minimum height of 1/5 of the maximum voltage
-    # Peaks must be at least 1.5 ms apart, if not they are considered from the same oscillation
-    height = np.max(1e6 * anatomical_muaps[:, i]) / 5
-    pos_peaks, _ = find_peaks(1e6 * anatomical_muaps[:, i], height=height, distance=1.5/dt) 
-    neg_peaks, _ = find_peaks(- 1e6 * anatomical_muaps[:, i], height=height, distance=1.5/dt)
-    peaks = np.sort(np.concatenate([pos_peaks, neg_peaks]))
-    amplitude = np.max(np.diff(peaks))
-
-    # If multiple crossing times find the crossing time between the 2 largest peaks
-    if len(crossings[i].times) > 1:
-        index = np.argmax(np.diff(peaks))
-        delay = crossings[i].times[index]
-    else:
-        delay = crossings[i].times
-
+popt, amplitude = amplitude_distribution(anatomical_muaps, t[1]-t[0])
+amplitude = 1e6 * amplitude
+for i in [1,18,32,35,49,76,92]:#range(100):
+    # Find the peaks of the anatomical MUAPs
+    peak_times = find_peak_times(anatomical_muaps[:, i], dt)
+    # Plot the anatomical MUAPs with highlighting the zero-crossing, the peaks and the amplitude
     plt.figure()
     plt.plot(t, 1e6 * anatomical_muaps[:, i])
-    plt.plot(t[peaks], 1e6 * anatomical_muaps[peaks, i], "r*")
+    plt.plot(t[peak_times], 1e6 * anatomical_muaps[peak_times, i], "r*")
     plt.plot(t, np.zeros(len(t)), "k--", linewidth=0.5)
-    plt.plot(crossings[i].times, np.zeros(len(crossings[i].times)), "m*")
-    plt.plot(delay, 0, "go")
+    #plt.plot(crossings[i].times, np.zeros(len(crossings[i].times)), "m*")
+    plt.plot(delay[i], 0, "go")
+    plt.plot([delay[i], delay[i]], [-amplitude[i], amplitude[i]], "--k")
     plt.xlim([0, t[-1]])
     plt.title("Shape of MUAP {0}".format(i))
     plt.xlabel("Time (ms)")
